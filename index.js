658771c0c87cb4ed36d9b1814cbf7c7413a24a73
bartimaeusrex <mrpatrickreinhart@gmail.com>
2020-04-17 14:05:15 -0400

    Excised needless comments

--- index.js
+++ index.js
@@ -3,16 +3,11 @@ const bodyParser = require('body-parser')
 const { getAllTeams, getTeamById, saveNewTeam } = require('./controllers/teams')
 const app = express()
 
-
 app.get('/teams', getAllTeams)
 
 app.get('/teams/:id', getTeamById)
 
 app.post('/teams', bodyParser.json(), saveNewTeam)
-
-// app.all('*', (request, response) => {
-//   return response.sendStatus(404).send('Oh no! Not found!')
-// })
 
 app.listen(8080, () => {
   // eslint-disable-next-line no-console
