a7759d903a87cac82780afc5a7ac06f736bb82ef
bartimaeusrex <mrpatrickreinhart@gmail.com>
2020-04-17 14:03:30 -0400

    Bits fixed, extra credit implemented as per video

--- controllers/teams.js
+++ controllers/teams.js
@@ -14,4 +14,63 @@ const getTeamById = (request, response) => {
     : response.sendStatus(404)
 }
 
-module.exports = { getAllTeams, getTeamById }
+const getNewId = () => {
+  const lastId = teams.reduce((acc, team) => {
+    return team.id > acc ? team.id : acc
+  }, 0)
+
+  return lastId + 1
+}
+
+const saveNewTeam = (request, response) => {
+  const {
+    location, mascot, abbreviation, conference, division
+  } = request.body
+
+  if (!location || !mascot || !abbreviation || !conference || !division) {
+    return response.status(400).send('All fields are required.')
+  }
+
+  const newTeam = {
+    location, mascot, abbreviation, conference, division, id: getNewId()
+  }
+
+  teams.push(newTeam)
+
+  return response.status(201).send(newTeam)
+}
+
+module.exports = { getAllTeams, getTeamById, saveNewTeam }
+
+
+/*
+
+const getAllTeams = (request, response) => {
+  return response.send(teams)
+}
+
+const getTeamById = (request, response) => {
+  const { id } = request.params
+
+  const foundTeam = teams.filter((team) => team.id === id)
+
+  return response.send(foundTeam)
+}
+
+const saveNewTeam = (request, response) => {
+  const { id, location, mascot, abbreviation, conference, division } = request.body
+
+  if (!id || !location || !mascot || !abbreviation || !conference || !division) {
+    return response.status(400).send('The following fields are required: id, location, mascot, abbreviation, conference, division')
+  }
+
+  const newTeam = { id, location, mascot, abbreviation, conference, division }
+
+  heroes.push(newTeam)
+
+  return response.status(201).send(newTeam)
+}
+
+module.exports = { getAllHeroes, getHeroBySlug, saveNewTeam }
+
+*/
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const { getAllTeams, getTeamById } = require('./controllers/teams')
+const bodyParser = require('body-parser')
+const { getAllTeams, getTeamById, saveNewTeam } = require('./controllers/teams')
 const app = express()
 
 
@@ -7,9 +8,11 @@ app.get('/teams', getAllTeams)
 
 app.get('/teams/:id', getTeamById)
 
-app.all('*', (request, response) => {
-  return response.sendStatus(404).send('Oh no! Not found!')
-})
+app.post('/teams', bodyParser.json(), saveNewTeam)
+
+// app.all('*', (request, response) => {
+//   return response.sendStatus(404).send('Oh no! Not found!')
+// })
 
 app.listen(8080, () => {
   // eslint-disable-next-line no-console
